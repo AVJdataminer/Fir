e26f9ab36ff87874b50dd90d0a1539ab6be34e51
Aiden V Johnson <aiden.dataminer@gmail.com>
2020-08-17 14:32:22 -0600

    Update Issues.py

--- Issues.py
+++ Issues.py
@@ -3,14 +3,13 @@ import pandas as pd
 import numpy as np
 import re
 import string
-from collections import Counter
-from gensim.summarization import keywords
 import matplotlib.pyplot as plt
+import altair as alt
 
 st.title('Job listing resume matcher')
 
 DATE_COLUMN = 'created_at'
-DATA_URL = ('https://raw.githubusercontent.com/AVJdataminer/Fir/master/data/DA_issues.csv')
+DATA_URL = ('https://raw.githubusercontent.com/AVJdataminer/Fir/master/data/simple_all_repo_issues.csv')
 
 @st.cache
 def load_data(nrows):
@@ -23,3 +22,11 @@ def load_data(nrows):
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
 data_load_state.text("Done! (using st.cache)")
+
+st.subheader('Number of issues per day')
+hist_values = np.histogram(data[DATE_COLUMN].dt.day, bins=7, range=(0,7))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('day', 0, 7, 1)
+filtered_data = data[data[DATE_COLUMN].dt.day == hour_to_filter]
